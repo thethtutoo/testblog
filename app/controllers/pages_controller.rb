eec2c5be747d5c02ca4ff2a2911f155e5cead04b
Thet Htut Oo <tho2410@gmail.com>
2017-08-26 13:09:40 +0630

    First Save

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+	def home
+		@name = "Thet Htut Oo"
+	end
+
+	def about
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do\
+  root 'pages#home'
+ 
+  get 'about' => 'pages#about'
+end
